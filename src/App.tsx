3b90d7f2984cef39505d51d48bcc6041677f81b8
Toms Ilzbergs <ilzbergs@inbox.lv>
2022-10-20 20:59:08 +0300

    added to heroku

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const GlobalStyle = createGlobalStyle`
 
   body, html {
     margin: 0 auto;
-    padding: 0;
+    padding: 0 px;
     width: 100%;
     height: 100vh;
     font-family: sans-serif;
